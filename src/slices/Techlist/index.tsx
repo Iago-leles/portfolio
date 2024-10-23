d769276282cef9d00d8936af346a0f9f72ef8864
iago.leles <iago.leles@gmail.com>
2024-10-23 17:40:56 -0300

    add tech list

--- src/slices/Techlist/index.tsx
+++ src/slices/Techlist/index.tsx
@@ -1,5 +1,8 @@
+import Heading from "@/components/Heading";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import React from "react";
+import { MdCircle } from "react-icons/md";
 
 /**
  * Props for `Techlist`.
@@ -15,7 +18,26 @@ const Techlist = ({ slice }: TechlistProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for techlist (variation: {slice.variation}) Slices
+      <Heading size="lg" as='h2'>
+        {slice.primary.heading}
+      </Heading>
+
+      {slice.primary.tech_list.map(({ tech_name, tech_color }, index) => (
+        <div key={index} className="tech-row mb-8 flex items-center justify-center gap-4 text-slate-700 overflow-hidden">
+          {Array.from({ length: 15 }, (_, index) => (
+            <React.Fragment key={index}>
+              <span className="tech-item text-8xl font-extrabold uppercase tracking-tighter"
+                style={{ color: index === 7 && tech_color ? tech_color : 'inherit' }}>
+                {tech_name}
+              </span>
+
+              <span>
+                <MdCircle className="text-3xl" />
+              </span>
+            </React.Fragment>
+          ))}
+        </div>
+      ))}
     </section>
   );
 };
